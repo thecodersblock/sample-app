f0bea4a864ff47714b0df18dde0eb0a8270d42ff
thecodersblock <thecodersblock@gmail.com>
2016-03-31 18:30:42 +0000

    Added user following/unfollowing

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,13 @@ Rails.application.routes.draw do
   
   
   get 'signup'  => 'users#new'
-  resources :users
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :account_activations, only: [:edit]
   
   
@@ -22,6 +28,7 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
 
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
